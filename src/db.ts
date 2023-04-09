213d22dac0bbbcf8f588a325e37854a0a0b78d28
Platon M <platon@miadvg.com>
2023-04-09 12:31:03 +0200

    Update env management

--- src/db.ts
+++ src/db.ts
@@ -2,13 +2,7 @@ import knex from "knex";
 
 const db = knex({
   client: 'pg',
-  connection: {
-    host: process.env.PSQL_HOST,
-    port: 5432,
-    user: process.env.PSQL_USER,
-    password: process.env.PSQL_PASSWORD,
-    database: process.env.PSQL_DATABASE
-  }
+  connection: process.env.DATABASE_URL
 })
 
 export default db
